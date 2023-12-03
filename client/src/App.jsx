9d1339a5d38fb8c329d5bf4b2b5e401480e6840a
emiltodorov21 <pacopoloviqorgan@gmail.com>
2023-12-03 15:05:34 +0200

    added comments and other stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import {Routes, Route} from 'react-router-dom'
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import GameList from './components/game-list/GameList'
+import GameCreate from './components/game-create/GameCreate'
+import Login from './components/login/Login'
+import Register from './components/register/register'
+import GameDetails from './components/game-details/GameDetails'
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<GameList />} />
+        <Route path="/games/create" element={<GameCreate />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/games/:gameId" element={<GameDetails />}  />
       </Routes>
       
       
