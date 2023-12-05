1a42955b7f40a62b8b63f19c28f8865f6b293b70
emiltodorov21 <pacopoloviqorgan@gmail.com>
2023-12-05 17:52:44 +0200

    added register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,55 @@
-import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+
+import * as authService from './services/authService'
+import AuthContext from './contexts/authContext'
+import Path from './paths'
+
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import GameList from './components/game-list/GameList'
 import GameCreate from './components/game-create/GameCreate'
 import Login from './components/login/Login'
-import Register from './components/register/register'
+import Register from './components/register/Register'
 import GameDetails from './components/game-details/GameDetails'
-import { useState } from 'react'
-import AuthContext from './contexts/authContext'
+
+
 
 function App() {
+  const navigate = useNavigate();
   const [auth, setAuth] = useState({});
 
-  const loginSubmitHandler = (values) => {
-    console.log(values);
+  const loginSubmitHandler = async (values) => {
+    const result = await authService.login(values.email, values.password);
+    setAuth(result);
+
+    navigate(Path.Home);
+  };
+
+  const registerSubmitHandler = async (values) => {
+    const result = await authService.register(values.email, values.password);
+
+    setAuth(result);
+
+    navigate(Path.Home);
+  };
+
+  const values = {
+    loginSubmitHandler,
+    registerSubmitHandler,
+    username: auth.username || auth.email,
+    email: auth.email,
+    isAuthenticated: !!auth.email,
   };
 
   return (
-    <AuthContext.Provider value={{loginSubmitHandler}}>
+    <AuthContext.Provider value={values}>
       <div id="box">
         <Header />
 
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path={Path.Home} element={<Home />} />
           <Route path="/games" element={<GameList />} />
           <Route path="/games/create" element={<GameCreate />} />
           <Route path="/login" element={<Login />} />
